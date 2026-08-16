026eaeb36d644f7b38d8a89c2a1bd54ad98440c7
Joshua Andrew <joshlfc1892@gmail.com>
2026-08-16 17:54:31 +0800

    Level-0 completed

---
+++ src/main/java/Bibi.java
@@ -0,0 +1,17 @@
+public class Bibi {
+    public static void main(String[] args) {
+        String banner = "B B B B    i    b b b    i\n"
+                + "B       B       b       b\n"
+                + "B B B B   iii   b b b b  iii\n"
+                + "B       B  i    b       b  i\n"
+                + "B B B B  iii   b b b b  iii\n"
+                + "Greetings Comrade! I'm Bibi, your ever present bot friend\n"
+                + "What can I do for you today?";
+        System.out.println(banner);
+
+        String exit = "Goodbye! Till next time..."
+        System.out.println(exit);
+
+
+    }
+}
