3b1de0c613cd000535ebcce4a13fdf435459e481
Joshua Andrew <joshlfc1892@gmail.com>
2026-08-16 18:16:05 +0800

    Implemented necessary changes for level 1

--- src/main/java/Bibi.java
+++ src/main/java/Bibi.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Bibi {
     public static void main(String[] args) {
         String banner = "B B B B    i    b b b    i\n"
@@ -6,11 +8,28 @@ public class Bibi {
                 + "B       B  i    b       b  i\n"
                 + "B B B B  iii   b b b b  iii\n"
                 + "Greetings Comrade! I'm Bibi, your ever present bot friend\n"
-                + "What can I do for you today?";
+                + "What can I do for you today? Type bye to exit \n";
+        String exit = "Goodbye! Till next time...";
         System.out.println(banner);
 
-        String exit = "Goodbye! Till next time..."
-        System.out.println(exit);
+        Scanner scan = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("You: ");
+            String input = scan.nextLine();
+
+            if (input.equals("bye") || input.equals("bye ")) {
+                System.out.println(exit);
+                break;
+            }
+
+            else {
+                System.out.println(input);
+            }
+        }
+
+        
+        
 
 
     }
